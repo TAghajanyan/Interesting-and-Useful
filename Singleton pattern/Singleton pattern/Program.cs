01404de9dc2026d3561982014b5008b717f62b9e
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-07-07 21:28:59 +0400

    Singleton pattern

---
+++ Singleton pattern/Singleton pattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance1 = Singleton.Instance();
+            Singleton instance2 = Singleton.Instance();
+
+            if (instance1 == instance2)
+            {
+                Console.WriteLine("instance1.GetHashCode() == instance2.GetHashCode()");
+            }
+            Console.WriteLine(instance1.GetHashCode());
+            Console.WriteLine(instance2.GetHashCode());
+        }
+    }
+}
---
+++ Singleton pattern/Singleton pattern/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_pattern
+{
+    class Singleton
+    {
+        private static Singleton instance = null;
+
+        public Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
