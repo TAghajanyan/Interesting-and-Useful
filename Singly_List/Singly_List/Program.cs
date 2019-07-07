07d62faaa0853c5864122c577d920ce7bc6e48a2
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-07-07 21:33:52 +0400

    Singly list

---
+++ Singly_List/Singly_List/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singly_List
+{
+    class Program
+    {
+        private static void PrintList(Node val) // val == first
+        {
+            while (val != null) //  val = first = new Node() != null 
+            {
+                Console.WriteLine(val.Value); // cw(first.Value == 1)
+                val = val.Next; // val = first.Next == sec 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Node first = new Node();
+            first.Value = 1;
+
+            Node sec = new Node();
+            sec.Value = 2;
+
+            first.Next = sec;
+
+            Node thr = new Node();
+            thr.Value = 3;
+
+            sec.Next = thr;
+
+            PrintList(first);
+        }
+    }
+}
