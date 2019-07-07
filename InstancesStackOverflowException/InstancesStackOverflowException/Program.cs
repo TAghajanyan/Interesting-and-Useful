5522a9a45e32c26d429376df15cca013dbb5e4ab
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-07-07 21:39:49 +0400

    Stack overflow exception

---
+++ InstancesStackOverflowException/InstancesStackOverflowException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstancesStackOverflowException
+{
+    class MyClass
+    {
+        public MyClass(int count)
+        {
+            count++;
+            Console.WriteLine(count);
+            MyClass my = new MyClass(count);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            MyClass myClass = new MyClass(count);
+        }
+    }
+}
