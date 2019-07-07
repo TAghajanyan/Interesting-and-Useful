a038fab463e9faf9b267e03c3387a07712d90f92
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-07-08 01:01:54 +0400

    Linked list

---
+++ LinkedListClass/LinkedListClass/LinkedList.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListClass
+{
+    class LinkedList<T>
+    {
+        private LinkedListNode<T> head;
+        private LinkedListNode<T> tail;
+        public int Count { get; private set; }
+
+        public void Print(LinkedList<T> instance, string str)
+        {
+            LinkedListNode<T> value = head;
+
+            if (value != null)
+            {
+                Console.WriteLine(str);
+
+                do
+                {
+                    Console.Write(value.Value + " ");
+                    value = value.Next;
+
+                } while (value != null);
+            }
+            else
+            {
+                Console.WriteLine("List is empty");
+            }
+            Console.WriteLine();
+        }
+
+        public void Add(T item)
+        {
+            LinkedListNode<T> node = new LinkedListNode<T>(item);
+
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+            Count++;
+        }
+
+        public bool Remove(T item)
+        {
+            LinkedListNode<T> previous = null;
+            LinkedListNode<T> current = head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+                        if (current.Next == null)
+                        {
+                            tail = previous;
+                        }
+                    }
+                    else
+                    {
+                        head = current.Next;
+
+                        if (head == null)
+                        {
+                            tail = null;
+                        }
+                    }
+                    Count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+
+        }
+
+        public T[] CopyTo(T[] arr, int index)
+        {
+            LinkedListNode<T> current = head;
+
+            while (current != null)
+            {
+                arr[index++] = current.Value;
+                current = current.Next;
+            }
+
+            return arr;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            Count = 0;
+        }
+    }
+}
---
+++ LinkedListClass/LinkedListClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListClass
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            LinkedList<int> instance = new LinkedList<int>();
+
+            instance.Add(15);
+            instance.Add(16);
+            instance.Add(17);
+
+            instance.Print(instance, "List");
+            Console.WriteLine("\nCount: " + instance.Count);
+
+            instance.Remove(15);
+            instance.Print(instance, "\nList (Removed '15')");
+            Console.WriteLine("\nCount: " + instance.Count);
+
+            instance.Add(150);
+
+            int[] arr = new int[100];
+            arr = instance.CopyTo(arr, 0);
+
+            Console.WriteLine("\nList items in array");
+            for (int i = 0; i < instance.Count; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            instance.Clear();
+            Console.WriteLine("\nList is cleared");
+
+            Console.ReadKey();
+        }
+    }
+}
