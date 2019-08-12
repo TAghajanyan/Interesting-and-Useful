e058ab428a3594fa92b30de6ac607f95382ffef2
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-12 11:28:37 +0400

    IEnumerable IEnumerator Foreach

--- IEnumerable, IEnumerator, Foreach/IEnumerable, IEnumerator, Foreach/Program.cs
+++ IEnumerable, IEnumerator, Foreach/IEnumerable, IEnumerator, Foreach/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IEnumerable__IEnumerator__Foreach
+namespace IEnumerable__IEnumerator__Foreach 
 {
     class Program
     {
