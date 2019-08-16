ad5012804e3c09535527b8b2b8732ded312c3c37
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-16 18:18:14 +0400

    DeleteAllRvtGarbageFiles

--- DeleteAllRvtGarbageFiles/DeleteAllRvtGarbageFiles/Program.cs
+++ DeleteAllRvtGarbageFiles/DeleteAllRvtGarbageFiles/Program.cs
@@ -9,21 +9,57 @@ namespace DeleteAllRvtGarbageFiles
 {
     class Program
     {
-        static void FindFiles(string folderPath, out List<FileInfo> files)
-        {
-            DirectoryInfo directory = new DirectoryInfo(folderPath);
-            files = new List<FileInfo>();
-            int x;
+        static int pos = 0;
 
+        static void FindeFolders(DirectoryInfo directory, ref List<FileInfo> files)
+        {
+            if (FindFiles(directory, ref files))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(new string(' ', pos) + directory.Name);
+                PrintFiles(directory);
+            }
+            else pos -= 3;
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            foreach (DirectoryInfo item in directory.GetDirectories())
+            {
+                try
+                {
+                    pos += 3;
+                    FindeFolders(item, ref files);
+                }
+                catch (PathTooLongException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        static bool FindFiles(DirectoryInfo directory, ref List<FileInfo> files)
+        {
+            int x;
+            string end = string.Empty;
+            bool b = false;
             foreach (FileInfo item in directory.GetFiles())
             {
-                string end = item.Name.Substring(item.Name.Length - 8, 4);
-
-                if (int.TryParse(end, out x) && item.Extension == ".rvt")
+                try
+                {
+                    end = item.Name.Substring(item.Name.Length - 8, 4);
+                }
+                catch { }
+                if (int.TryParse(end, out x) && item.Extension == ".rvt" && item.Name[item.Name.Length - 9] == '.')
                 {
                     files.Add(item);
+                    b = true;
                 }
-            }                
+            }
+            return b;
         }
 
         static void DeleteFiles(List<FileInfo> files)
@@ -34,18 +70,13 @@ namespace DeleteAllRvtGarbageFiles
                 return;
             }
 
-            Console.ForegroundColor = ConsoleColor.Red;
-            foreach (FileInfo item in files)
-            {
-                Console.WriteLine(item.FullName);
-            }
-            Console.ForegroundColor = ConsoleColor.Gray;
-
             Console.WriteLine(new string('-', 60));
+
             Console.WriteLine("Do you really want to delete this files ['Y': Yes, 'N': No ]");
 
             flag:
             string answer = Console.ReadLine();
+
             if (answer == "Y")
             {
                 foreach (FileInfo item in files)
@@ -64,11 +95,31 @@ namespace DeleteAllRvtGarbageFiles
                 goto flag;
             }
         }
+        static void PrintFiles(DirectoryInfo directory)
+        {
+            int x;
+            string end = string.Empty;
+
+            foreach (FileInfo item in directory.GetFiles())
+            {
+                try
+                {
+                    end = item.Name.Substring(item.Name.Length - 8, 4);
+                }
+                catch { }
+                if (int.TryParse(end, out x) && item.Extension == ".rvt" && item.Name[item.Name.Length - 9] == '.')
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(new string(' ', pos + 3) + item.Name);
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
-            List<FileInfo> files;
-            string path = null;
+            List<FileInfo> files = new List<FileInfo>();
+            DirectoryInfo directory;
+            string path = string.Empty;
 
             Console.WriteLine("Please input path. For example 'D:\\Users\\Revit\\MyProjects' (For exit input 'E')");
 
@@ -76,19 +127,23 @@ namespace DeleteAllRvtGarbageFiles
             {
                 Console.Write("Path -> ");
                 path = Console.ReadLine();
+
                 if (path == "E")
                     break;
+
 
                 Console.WriteLine(new string('-', 60));
                 try
                 {
-                    FindFiles($@"{path}", out files);
+                    directory = new DirectoryInfo(path);
+                    Console.WriteLine(path);
+                    FindeFolders(directory, ref files);
                     DeleteFiles(files);
                 }
                 catch (Exception)
                 {
+                    Console.ForegroundColor = ConsoleColor.Gray;
                     Console.WriteLine("Wrong path, please input existing path!");
-                    files = null;
                 }
             } while (true);
 
