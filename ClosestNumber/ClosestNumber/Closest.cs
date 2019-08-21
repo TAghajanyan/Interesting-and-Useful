8f608d920b92962887e4959b42296db6eadb4eb7
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-21 22:54:09 +0400

    ClosestNumber

---
+++ ClosestNumber/ClosestNumber/Closest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClosestNumber
+{
+    class Closest
+    {
+        private List<double> numbers = new List<double>();
+        private int index;
+
+        public void Input(double number)
+        {
+            numbers.Add(number);
+        }
+
+        public double GetResult()
+        {
+            double current = numbers[numbers.Count - 1];
+            numbers.Sort();
+            index = numbers.IndexOf(current);
+
+            if (index == 0)
+            {
+                return numbers[index + 1];
+            }
+            else if (index == numbers.Count - 1)
+            {
+                return numbers[index - 1];
+            }
+            else if (Math.Abs(numbers[index] - numbers[index - 1]) < Math.Abs(numbers[index] - numbers[index + 1]))
+            {
+                return numbers[index - 1];
+            }
+            else
+            {
+                return numbers[index + 1];
+            }
+        }
+    }
+}
+
---
+++ ClosestNumber/ClosestNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClosestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Closest my = new Closest();
+            bool b = false;
+
+            Console.WriteLine("Input numbers. [e for exit!]");
+            do
+            {
+                try
+                {
+                    my.Input(Convert.ToDouble(Console.ReadLine()));
+                    b = true;
+                }
+                catch (FormatException)
+                {
+                    break;
+                }
+
+            } while (true);
+
+            if (b)
+            {
+                Console.WriteLine("Input your number.");
+                my.Input(Convert.ToDouble(Console.ReadLine()));
+
+                Console.WriteLine($"Result: {my.GetResult()}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
