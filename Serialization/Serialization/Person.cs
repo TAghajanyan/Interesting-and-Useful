6da735253d23e0e6d54a27b19f347aed4d39e12c
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-22 23:41:23 +0400

    Serialization ISerializable

---
+++ Serialization/Serialization/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Person : ISerializable
+    {
+        [NonSerialized]
+        private int id = 254463;
+
+        public string FirstName { get; private set; } = "John";
+        public string LastName { get; private set; } = "Smith";
+
+        public Person()
+        {
+        }
+
+        private Person(SerializationInfo info, StreamingContext context)
+        {
+            FirstName = info.GetString("FirstName");
+            LastName = info.GetString("LastName");
+        }
+
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("FirstName", FirstName);
+            info.AddValue("LastName", LastName);
+        }
+    }
+}
---
+++ Serialization/Serialization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Stream stream = File.Create("Person.xml");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(stream, person);
+            stream.Close();
+
+            stream = File.OpenRead("Person.xml");
+            person = formatter.Deserialize(stream) as Person;
+
+            Console.WriteLine("First name: " + person.FirstName + "\nLast name: " + person.LastName);
+
+            Console.ReadKey();
+
+        }
+    }
+}
