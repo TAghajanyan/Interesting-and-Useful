27813d56e2437a6c53f89a443ee54a5f654d4795
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-22 18:39:48 +0400

    ClosestNumber

--- ClosestNumber/ClosestNumber/Program.cs
+++ ClosestNumber/ClosestNumber/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ClosestNumber
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        public static void Main(string[] args)
         {
-            Closest my = new Closest();
+            //Variant 1.
+
+            //double[] nums = { 1.2, 1.5, 2.5, 2.8, 3, 3.9, 8, 10, 15, 20, 25 };
+            //double num = Convert.ToDouble(Console.ReadLine());
+
+            //double res = nums[0];
+            //double min = Math.Abs(num - nums[0]);
+
+            //for (int i = 1; nums[i++] < num && i < nums.Length;)
+            //{
+            //    if (Math.Abs(nums[i] - num) < min)
+            //    {
+            //        min = Math.Abs(nums[i] - num);
+            //        res = nums[i];
+            //    }
+            //}
+
+            //Console.WriteLine(res);
+            //Console.ReadKey();
+            
+            // ----------------------------------------------------------------------
+
+
+            //Variant 2.
+
+            Closest closest = new Closest();
             bool b = false;
 
             Console.WriteLine("Input numbers. [e for exit!]");
@@ -14,7 +40,7 @@ namespace ClosestNumber
             {
                 try
                 {
-                    my.Input(Convert.ToDouble(Console.ReadLine()));
+                    closest.Input(Convert.ToDouble(Console.ReadLine()));
                     b = true;
                 }
                 catch (FormatException)
@@ -27,9 +53,9 @@ namespace ClosestNumber
             if (b)
             {
                 Console.WriteLine("Input your number.");
-                my.Input(Convert.ToDouble(Console.ReadLine()));
+                closest.Input(Convert.ToDouble(Console.ReadLine()));
 
-                Console.WriteLine($"Result: {my.GetResult()}");
+                Console.WriteLine($"Result: {closest.GetResult()}");
             }
 
             Console.ReadKey();
